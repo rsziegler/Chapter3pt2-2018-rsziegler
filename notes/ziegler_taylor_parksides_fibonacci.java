ed17d8a9d0303c53b4cdb33ae8a71328a1919467
rsziegler <33166821+rsziegler@users.noreply.github.com>
2018-10-20 15:46:57 -0500

    actually finally done

--- notes/ziegler_taylor_parksides_fibonacci.java
+++ notes/ziegler_taylor_parksides_fibonacci.java
@@ -10,24 +10,39 @@ import java.util.Scanner;
 
 public class ziegler_taylor_parksides_fibonacci
 {
-    public static void main(String [] args) {
-        int x = 0, y = 1;
+    public static void main(String [] args) {      
         Scanner scan = new Scanner(System.in);
-        System.out.println("How far would you like to count? (enter a number) ");
-        int lim = scan.nextInt();
-        lim -= 2;
-        System.out.println(x);
-        System.out.println(y);
+        System.out.println("Size? ");
+        int n = scan.nextInt();
+        System.out.println("Seed? ");
+        int s = scan.nextInt();
+        int x = s;
         
-        for (int i = 0; i < lim; i += 1) {
-            if (i%2 == 0) {
-                x = x + y;
-                System.out.println(x);
-            }
-            else {
-                y = y + x;
-                System.out.println(y);
-            }
+        if ((s < 1) || (s > 9)) {
+            System.out.println("Not an acceptable seed");
         }
+        
+        // prints out spaces before the numbers in each row
+        for (int row = 0; row < n; row++) {
+            for (int spaces = 0; spaces < row*2; spaces++) {
+                System.out.print(" ");
+            }
+            x = s;
+            
+            // calculates and prints out numbers with a space between each number
+            for (int col = 0; col < n - row; col++) {
+                System.out.print(((x - 1) % 9) + 1);
+                if (col != (n - row - 1)) {
+                    System.out.print(" ");
+                    x += row + col + 1;
+                }
+            }
+            s += row + 2;
+            System.out.println("");
+        }
+        
+        
+        
+        
     }
 }
